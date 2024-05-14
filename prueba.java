9275a698e26e8b3474283d00bc37853789117ad8
Pedro Plana Turmo <pedro.plana@movicoders.com>
2024-05-14 10:11:09 +0200

    security try

--- prueba.java
+++ prueba.java
@@ -79,3 +79,42 @@ private static final String[] WHITE_LIST_URL = { "/api/v1/auth/**",
 
         return http.build();
         }
+
+        
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
+        Exception {
+        http
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests (authorizeHttpRequests ->
+    authorizeHttpRequests
+        //.requestMatchers("/**").hasRole("USER")
+    .requestMatchers("/api/auth/login/**",
+        "/v2/api-docs",
+        "/v3/api-docs",
+        "/v3/api-docs/**",
+        "/swagger-resources",
+        "/swagger-resources/**",
+        "/configuration/ui",
+        "/configuration/security",
+        "/swagger-ui/**",
+        "/webjars/**",
+        "/swagger-ui.html").permitAll()
+        .anyRequest().authenticated()
+        )
+        .permitAll()
+        and().anyRequest()
+        .authenticated()
+        .sessionManagement(session -> session
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authenticationProvider(authenticationProvider)
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+        .logout(logout -> logout.logoutUrl("/api/v1/auth/logout")
+        .addLogoutHandler(logoutHandler)
+        .logoutSuccessHandler(
+        (request, response,
+        authentication) -> SecurityContextHolder
+        .clearContext()));
+
+        return http.build();
+        }
